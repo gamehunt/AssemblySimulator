1c48dade1160685c960ab8dbe2ed10b75f918a60
Lizord <mt.demakov@gmail.com>
2024-11-23 15:50:44 +0300

    whoops

--- libs/Assemblies/AMD64/amd64syntaxhighlighter.cpp
+++ libs/Assemblies/AMD64/amd64syntaxhighlighter.cpp
@@ -43,11 +43,8 @@ AMD64SyntaxHighlighter::AMD64SyntaxHighlighter(QTextDocument* parent)
         QStringLiteral("\\bint\\b" ),
         QStringLiteral("\\btest\\b"),
         QStringLiteral("\\blea\\b"),
-<<<<<<< HEAD
-=======
         QStringLiteral("\\bnot\\b"),
         QStringLiteral("\\bneg\\b")
->>>>>>> d13434b2eee090b3b244d7e245ffc6bed15b55b2
     };
 
     for (const QString &pattern : opPatterns) {
